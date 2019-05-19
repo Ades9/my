32cac302cc713cd329455faee29e4b5f7ed8d85a
Your Name <you@example.com>
2019-05-19 14:47:21 +0400

    pp

---
+++ Napastak.js
@@ -0,0 +1,124 @@
+class Napastak extends LivingCreature {
+    constructor(x, y, index) {
+        super(x, y, index);
+        this.energy = 12;
+        this.directions = [];
+        this.acted = false;
+
+    }
+    getNewCoordinates() {
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+
+    chooseCell(num) {
+        this.getNewCoordinates();
+        var found = [];
+        for (var i in this.directions) {
+            var x = this.directions[i][0];
+            var y = this.directions[i][1];
+            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
+                if (matrix[y][x] == num) {
+                    found.push(this.directions[i]);
+                }
+                else if (matrix[y][x].index == num) {
+                    found.push(this.directions[i]);
+                }
+            }
+        }
+        return found;
+    }
+
+    move() {
+        var newCord = random(this.chooseCell(0));
+        if (this.acted == false) {
+            if (newCord) {
+                var newX = newCord[0];
+                var newY = newCord[1];
+
+                matrix[newY][newX] = matrix[this.y][this.x];
+                matrix[this.y][this.x] = 0;
+
+                this.x = newX;
+                this.y = newY;
+                this.acted = true;
+                this.energy--;
+                if (this.energy <= 0) {
+                    this.die();
+                }
+            }
+        }
+    }
+    mul() {
+        this.multiply++;
+        var newCord = random(this.chooseCell(0));
+
+        if (newCord && this.multiply >= 8) {
+            var newX = newCord[0];
+            var newY = newCord[1];
+
+            matrix[newY][newX] = new Napastak(newX, newY, 4);
+            this.energy--;
+            this.multiply = 0;
+        }
+    }
+    eat() {
+        if (this.acted == false) {
+            var newCord = random(this.chooseCell(2));
+
+            if (newCord) {
+                var newX = newCord[0];
+                var newY = newCord[1];
+
+                matrix[newY][newX] = matrix[this.y][this.x];
+                matrix[this.y][this.x] = 0;
+
+                this.x = newX;
+                this.y = newY;
+
+                this.energy++;
+                this.acted = true;
+                if (this.energy >= 12) {
+                    this.mul();
+                }
+            }
+            else {
+                var newdir = random(this.chooseCell(1));
+
+                if (newdir) {
+                    var newX = newdir[0];
+                    var newY = newdir[1];
+
+                    matrix[newY][newX] = matrix[this.y][this.x];
+                    matrix[this.y][this.x] = 0;
+
+                    this.x = newX;
+                    this.y = newY;
+
+                    this.energy++;
+                    this.acted = true;
+                    if (this.energy >= 12) {
+                        this.mul();
+                    }
+                }
+                else {
+                    this.move();
+                }
+            }
+
+        }
+
+    }
+    die() {
+
+        matrix[this.y][this.x] = 0;
+    }
+}
